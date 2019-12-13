4585d54a23e77680563def97d4c981d4bb89e02e
Janina Hard <hard@werkbank.de>
2019-12-13 11:11:41 +0100

    "Add vocabulary: CountryInformation"

---
+++ src/collective/contactinformation/tests/test_vocab_country_information.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from collective.contactinformation import _
+from collective.contactinformation.testing import COLLECTIVE_CONTACTINFORMATION_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.interfaces import IVocabularyTokenized
+
+import unittest
+
+
+class CountryInformationIntegrationTest(unittest.TestCase):
+
+    layer = COLLECTIVE_CONTACTINFORMATION_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_vocab_country_information(self):
+        vocab_name = 'collective.contactinformation.CountryInformation'
+        factory = getUtility(IVocabularyFactory, vocab_name)
+        self.assertTrue(IVocabularyFactory.providedBy(factory))
+
+        vocabulary = factory(self.portal)
+        self.assertTrue(IVocabularyTokenized.providedBy(vocabulary))
+        self.assertEqual(
+            vocabulary.getTerm('sony-a7r-iii').title,
+            _(u'Sony Aplha 7R III'),
+        )
