c1972c3cf751da2e6db40b2e584872b3a3f513c2
Janina Hard <hard@werkbank.de>
2019-12-20 14:04:26 +0100

    Programmierung des Macros zur Anzeige von Kontaktinformationen

--- src/collective/contactinformation/views/contacts_macro.py
+++ src/collective/contactinformation/views/contacts_macro.py
@@ -3,15 +3,27 @@
 from collective.contactinformation import _
 from Products.Five.browser import BrowserView
 
-
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
+# Import für Api
+from plone import api
 
 class ContactsMacro(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('contacts_macro.pt')
-
     def __call__(self):
-        self.msg = _(u'A small message')
         return self.index()
+
+    def getContactReferences(self):
+        context = self.context
+        # Kontaktinformationen abrufen
+        contacts = context.relatedContacts
+        # leere Liste für korrekte Verlinkungen
+        correct_contacts = []
+        if contacts:
+            for contact in contacts:
+                # Prüfen ob eine der möglichen Verlinkungen kaputt ist
+                # wenn nicht kaputt, in Liste schreiben für Ausgabe
+                if contact.isBroken():
+                    pass
+                else:
+                    correct_contacts.append(contact)
+            return correct_contacts
+        else:
+            return None
