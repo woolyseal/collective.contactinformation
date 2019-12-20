d64ddd09e9711a3b4766bb2daccd89fe040d0ee5
Janina Hard <hard@werkbank.de>
2019-12-20 09:53:59 +0100

    "Add view: contacts-macro"

---
+++ src/collective/contactinformation/tests/test_view_contacts_macro.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from collective.contactinformation.testing import COLLECTIVE_CONTACTINFORMATION_FUNCTIONAL_TESTING
+from collective.contactinformation.testing import COLLECTIVE_CONTACTINFORMATION_INTEGRATION_TESTING
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getMultiAdapter
+from zope.component.interfaces import ComponentLookupError
+
+import unittest
+
+
+class ViewsIntegrationTest(unittest.TestCase):
+
+    layer = COLLECTIVE_CONTACTINFORMATION_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(self.portal, 'Folder', 'other-folder')
+        api.content.create(self.portal, 'Document', 'front-page')
+
+    def test_contacts_macro_is_registered(self):
+        view = getMultiAdapter(
+            (self.portal['other-folder'], self.portal.REQUEST),
+            name='contacts-macro'
+        )
+        self.assertTrue(view.__name__ == 'contacts-macro')
+        # self.assertTrue(
+        #     'Sample View' in view(),
+        #     'Sample View is not found in contacts-macro'
+        # )
+
+    def test_contacts_macro_not_matching_interface(self):
+        with self.assertRaises(ComponentLookupError):
+            getMultiAdapter(
+                (self.portal['front-page'], self.portal.REQUEST),
+                name='contacts-macro'
+            )
+
+
+class ViewsFunctionalTest(unittest.TestCase):
+
+    layer = COLLECTIVE_CONTACTINFORMATION_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
