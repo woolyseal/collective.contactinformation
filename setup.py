63ba249ea4f8d5d4aef7d93094bfcd3c9824a073
Janina Hard <hard@werkbank.de>
2019-12-20 15:16:53 +0100

    Set new Release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.contactinformation',
-    version='1.0a1',
+    version='1.1',
     description="AddOn for Plone to manage contactinformation related to content or on worldmap",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
